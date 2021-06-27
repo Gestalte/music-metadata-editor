2186f83d644101eb4d74c0cc9a7ef6616afdecd9
Gestalte <philipbenade@gmail.com>
2021-06-27 17:17:32 +0200

    Changed target framework and made it so that files are populated from a filepath and that selecting a file shows it tags.

--- windows-music-metadata-editor/MainWindow.xaml.cs
+++ windows-music-metadata-editor/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Id3;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace windows_music_metadata_editor
 {
@@ -11,8 +14,11 @@ namespace windows_music_metadata_editor
     {
         public class Tags
         {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
             public string Title { get; set; }
             public string Artists { get; set; }
+            public string Artist { get; set; }
             public string Album { get; set; }
             public string Track { get; set; }
         }
@@ -20,12 +26,13 @@ namespace windows_music_metadata_editor
         public MainWindow()
         {
             InitializeComponent();
-
-            //test();
+            this.Loaded += MainWindow_Loaded;
         }
 
-        public void test()
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            lbxFiles.Items.Clear();
+
             string path = @"";
 
             string[] musicFiles = Directory.GetFiles(path, "*.mp3");
@@ -36,15 +43,52 @@ namespace windows_music_metadata_editor
                 {
                     Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
 
-                    var tags = new Tags
+                    var tagObject = new Tags
                     {
-                        Album = tag.Album,
-                        Artists = tag.Artists,
-                        Title = tag.Artists,
-                        Track = tag.Track,
+                        FileName = Path.GetFileName(musicFile),
+                        FilePath = musicFile
                     };
+
+                    if (tag != null)
+                    {
+                        tagObject.Album = string.IsNullOrEmpty(tag.Album) ? "" : tag.Album.ToString();
+                        tagObject.Artists = string.IsNullOrEmpty(tag.Artists) ? "" : tag.Artists.ToString();
+                        tagObject.Artist = string.IsNullOrEmpty(tag.Band) ? "" : tag.Band.ToString();
+                        tagObject.Title = string.IsNullOrEmpty(tag.Title) ? "" : tag.Title.ToString();
+                        tagObject.Track = string.IsNullOrEmpty(tag.Track) ? "" : tag.Track.ToString();
+                    }
+
+                    lbxFiles.Items.Add(tagObject);
                 }
             }
+        }
+
+        private void lbxFiles_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Action<List<TextBox>> clearTextBoxes = lst
+                => lst.ForEach(b => b.Text = "");
+
+            clearTextBoxes(new List<TextBox>
+            {
+                tbxAlbum,
+                tbxArtist,
+                tbxCover,
+                tbxTitle,
+                tbxTrackNo,
+                tbxYear
+            });
+
+            var tags = lbxFiles.SelectedItem as Tags;
+
+            tbxAlbum.Text = tags.Album;
+            tbxArtist.Text = tags.Artist;
+            tbxTitle.Text = tags.Title;
+            tbxTrackNo.Text = tags.Track;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Save tags here.
         }
     }
 }
